2194faaf0d999e5a5bfd4713ab8fc2e96a6fd1ea
Adam Grant <hello@adamgrant.me>
2022-08-10 15:41:27 -0700

    Ok, now I'm bored

--- permutation_topics/corporate_apology/sorry.js
+++ permutation_topics/corporate_apology/sorry.js
@@ -1,20 +1,33 @@
 permutations.sorry = {
   "main": [
-    { "branch": "subject", "then": [
-      " are ", [
-        { "branch": "sorry", "then": {
-          "branch": "for the stuff"
-        } }
+    {
+      "branch": "subject",
+      "then": [
+        " are ",
+        [
+          {
+            "branch": "sorry",
+            "then": {
+              "branch": "for the stuff"
+            }
+          }
+        ]
       ]
-    ] } 
+    }
   ],
   "subject": [
-    "We", "Here at Acme Inc, we", "All of us here at Acme"
+    "We",
+    "Here at Acme Inc, we",
+    "All of us here at Acme"
   ],
   "sorry": [
-    { "ps": "(very |extremely |profoundly |)(regretful|sorry) " }
+    {
+      "ps": "(very |extremely |profoundly |)(regretful|sorry) "
+    }
   ],
   "for the stuff": [
-    {"ps": "(for|in light of|hearing) the (news|recent events|recent reports) that have (transpired|appeared in the news|come up in the media)." }
+    {
+      "ps": "(for|in light of|hearing) the (news|recent events|recent reports) that have (transpired|appeared in the news|come up in the media)."
+    }
   ]
-};
+} 
--- permutation_topics/corporate_apology/will_fix.js
+++ permutation_topics/corporate_apology/will_fix.js
@@ -8,7 +8,7 @@ permutations.will_fix = {
       }]
     } 
   ],
-  "subject": ["We"],
-  "are-doing-stuff": { "ps": "are (taking steps) to (review) (how the) (situation) was (handled)." },
+  "subject": ["We", "Here at Acme, we", "All of us here at Acme"],
+  "are-doing-stuff": { "ps": "are (taking steps|forming a plan|following protocol) to (review|revisit|examine|measure) (how the|the way the|the details of how the) (situation|incident|adverse event) was (handled|actioned|deployed)." },
   "and-stuff-will-be-better": { "ps": "(We) (believe) (in our) (commitment) to (doing what's right)." }
 };
