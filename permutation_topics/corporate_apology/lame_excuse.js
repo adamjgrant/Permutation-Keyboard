06f29fa020f5ed102c3a3c4bc2a0aef8dde3f6fe
Adam Grant <hello@adamgrant.me>
2022-08-10 12:35:48 -0700

    Scaffolding more excuses

---
+++ permutation_topics/corporate_apology/lame_excuse.js
@@ -0,0 +1,11 @@
+permutations.lame_excuse = {
+  "main": [
+    { 
+      "branch": "cuz we give a shit", "then": [" ", {
+        "branch": "training is happening, lol"
+      }]
+    }
+  ],
+  "cuz we give a shit": { "ps": "(To demonstrate) (our commitment) to (doing what's right), " },
+  "training is happening, lol": { "ps": "all (employees) will (be enrolled in) (mandatory) (sensitivity) (training) (starting) (next week)" }
+};
---
+++ permutation_topics/corporate_apology/will_fix.js
@@ -0,0 +1,14 @@
+permutations.will_fix = {
+  "main": [
+    { 
+      "branch": "subject", "then": [" ", {
+        "branch": "are-doing-stuff", "then": [" ", {
+          "branch": "and-stuff-will-be-better"
+        }]
+      }]
+    } 
+  ],
+  "subject": ["We"],
+  "are-doing-stuff": { "ps": "are (taking steps) to (review) (how the) (situation) was (handled)." },
+  "and-stuff-will-be-better": { "ps": "(We) (believe) (in our) (commitment) to (doing what's right)." }
+};
