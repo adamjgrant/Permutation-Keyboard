40436dad1100c6280067843ddd1068653ee0dee3
Adam Grant <hello@adamgrant.me>
2022-08-10 14:51:13 -0700

    Adding more to lame excuse

--- permutation_topics/corporate_apology/lame_excuse.js
+++ permutation_topics/corporate_apology/lame_excuse.js
@@ -4,8 +4,17 @@ permutations.lame_excuse = {
       "branch": "cuz we give a shit", "then": [" ", {
         "branch": "training is happening, lol"
       }]
-    }
+    },
+    [
+      "We ", [
+        "understand we're not perfect", [
+          " and ", [
+            "don't always get it right", ["."]
+          ]
+        ]
+      ]
+    ]
   ],
-  "cuz we give a shit": { "ps": "(To demonstrate) (our commitment) to (doing what's right), " },
-  "training is happening, lol": { "ps": "all (employees) will (be enrolled in) (mandatory) (sensitivity) (training) (starting) (next week)" }
+  "cuz we give a shit": { "ps": "(In order to|As our way|To) (demonstrate|prove|show) (our|Acme's) (commitment to|perseverance for) (doing what's right|doing the right thing|holding ourselves accountable|staying accountable), " },
+  "training is happening, lol": { "ps": "(we're announcing |)(today |)all (employees|staff) will (be enrolled in|be signed up|participate in|benefit from) (mandatory|compulsory) (sensitivity|customer service) training(| programs) (starting|beginning) (next week|today|tomorrow|at the start of the month)" }
 };
