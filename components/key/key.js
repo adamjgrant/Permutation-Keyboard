3e323a52049ca9ec7a96498b3be157c4ff65fe2c
Adam Grant <hello@adamgrant.me>
2022-08-10 11:38:23 -0700

    Key stuff almost ready to work

--- components/key/key.js
+++ components/key/key.js
@@ -1,5 +1,21 @@
 import Component from "../../mozart.js";
+import Tree from "../../permute.js";
 
 let key = new Component("key");
+
+key.assign({
+    insert_permutation(e) {
+        let permutation_name = this.get_permutation_name(e);
+        console.log(permutation_name)
+    },
+
+    get_permutation_name(e) {
+        let element = e.target;
+        e.stopPropagation();
+        return element.dataset.permutation;
+    }
+});
+
+[].concat(key.me).forEach(_key => _key.addEventListener("click", key.insert_permutation));
 
 export default key;
